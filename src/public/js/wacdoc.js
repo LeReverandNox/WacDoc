1adfa17a7c235802a4b7b7a3953395e4aeaceca4
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-20 04:04:56 +0200

    Begin to work on the frontend app...

--- src/public/js/wacdoc.js
+++ src/public/js/wacdoc.js
@@ -1,1 +1,39 @@
-console.log("Hello, World !");
+/*jslint this:true es6:true for:true */
+/*global window document io $ randomColor*/
+
+"use strict";
+
+(function (global) {
+    var WacDoc = function () {
+        this.fileUUID = window.location.href.match(/\/edit\/(.*)$/)[1];
+    };
+
+    WacDoc.prototype.init = function () {
+        var self = this;
+
+        this.getTextContent(function () {
+            console.log(self.originalContent);
+            self.launchEditor();
+        });
+    };
+
+    WacDoc.prototype.getTextContent = function (cb) {
+        var self = this;
+        var url = "/gettextcontent/" + this.fileUUID;
+        $.get(url, function (res) {
+            self.originalContent = res.content;
+            cb();
+        });
+    };
+
+    WacDoc.prototype.launchEditor = function () {
+        console.log("C'est ti-part pour le RTE");
+    };
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var wd = new WacDoc();
+        wd.init();
+    });
+
+    global.WacDoc = WacDoc;
+}(window));
